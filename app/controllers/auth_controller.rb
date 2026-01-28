389c32ee16df8e1e9b6b61004b118548c68a6f65
Arshan <arshankhan9326863763@gmail.com>
2026-01-28 11:20:02 +0530

    No Authentication for testing

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,5 @@
 class AuthController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:login]
+    # skip_before_action :authenticate_user!, only: [:login]
   
     def login
       user = User.find_by(email: params[:email])
